0e366c64021fc971762c18a6daafbd82e9efa753
Aryan9788 <74713058+Aryan9788@users.noreply.github.com>
2021-03-21 13:19:21 +0530

    Add files via upload

---
+++ carRacingStage0.5-master/js/Game.js
@@ -0,0 +1,49 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+ async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef=await database.ref('playerCount').once("value")
+      if (playerCountRef.exists()){
+        playerCount=playerCountRef.val()
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+  }
+  play(){
+    form.hide()
+    textSize(50)
+    text("GameStart",120,80)
+    Player.getplayerinfo()
+    if(allPlayers!==undefined){
+      var display=130
+      for(var plr in allPlayers){
+        display+=20
+        if (plr ==="player"+player.index)
+        fill("Blue")
+        else 
+        fill("Black")
+        textSize(20)
+        text (allPlayers[plr].name+":"+allPlayers[plr].distance,120,display)
+      }
+      
+    }
+}
+}
